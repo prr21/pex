462ce010e96c90a96ca8a4e7437559605b0e1d01
Kurban <Mr.gustav009@gmail.com>
2020-10-05 18:12:44 +0300

    Final edits

--- build/index.js
+++ build/index.js
@@ -3,6 +3,7 @@ const header = document.querySelector('.header-content')
 const menuIcon = menu.firstChild
 
 menu.onclick = () => toggleMenu()
+window.onscroll = scrollControlInit()
 
 document.body.onclick = e => {
   if (!e.target.closest('.header-content') && header.classList.contains('open')){
@@ -21,5 +22,20 @@ function toggleMenu(){
   } else {
     document.body.style.overflow = 'auto'
     menuIcon.className = 'fa fa-bars'
+  }
+}
+
+function scrollControlInit(){
+  let initialOffset = window.pageYOffset
+
+  return () => {
+    let currentOffset = window.pageYOffset
+
+    if (currentOffset > initialOffset){
+      header.style.top = '-100px'
+      
+    } else header.style.top = '0'
+
+    initialOffset = currentOffset
   }
 }
